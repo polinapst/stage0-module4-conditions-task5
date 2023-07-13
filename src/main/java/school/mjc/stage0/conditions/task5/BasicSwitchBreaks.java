7ac309ae46dace830e1259d8fc83bd44798e7009
polina <polinatuzankina@gmail.com>
2023-07-13 21:42:03 +0600

    module 4 task 5 - switch

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -4,14 +4,18 @@ public class BasicSwitchBreaks {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
-            case 7:
+            case (7):
                 System.out.println("seven");
-            case 10:
+                break;
+            case (10):
                 System.out.println("ten");
-            case 20:
+                break;
+            case (20):
                 System.out.println("twenty");
+                break;
             default:
                 System.out.println("default");
+                break;
         }
     }
 }
